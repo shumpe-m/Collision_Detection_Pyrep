5f551e4ce7a06d49646f0374222a821e660fb56b
Shunpei Morita <65478897+shunpe-hash@users.noreply.github.com>
2022-08-30 16:20:14 +0900

    Cuboidの山を作成することが可能。

--- Collision_detection.py
+++ Collision_detection.py
@@ -1,12 +1,11 @@
 import time
-
 import os.path as osp
 from pyrep import PyRep
 from pyrep.objects.shape import Shape
+from pyrep.objects.object import Object
 from pyrep.const import PrimitiveShape
 import numpy as np
 import quaternion
- 
 
 class PyrepEnv:
     def __init__(self, scene_file: str, headless=True):
@@ -43,75 +42,70 @@ class CollisionDetectionEnv(PyrepEnv):
 
         self.pr.start()
         [self.pr.step() for _ in range(200)]
+        #self.initial_num_objects, _ = self._num_objects(0)
+    
+    '''
+    # scene 内のオブジェクトの数と名前を返す
+    def _num_objects(self, count):
+        done = True
+        while done:
+            ob = Object.get_object_name(count)
+            done = False if ob == '' else True
+            print(ob)
+            count += 1
+        return count - 1, Object.get_object_name(count-2)
+    '''
 
-    def _get_state(self):
-        return Shape('Cuboid').get_position(), Shape('Cuboid').get_orientation()
+    def _get_state(self, obj_name):
+        return Shape(obj_name).get_position(), Shape(obj_name).get_orientation()
 
 
     def _collision_detection(self):
+        # terminalで'yes'が入力されるまで繰り返す
         while not self.done:
             print('----Create a Arrangement----')
+            # num_obj の個数生成
             for obj in range(self.num_obj):
                 # size = list(np.random.uniform(self.obj_size_min, self.obj_size_max))
                 pos = list(np.random.uniform(self.position_min, self.position_max))
                 ori = list(np.random.uniform(self.orientation_min, self.orientation_max))
-                self._create_object(pos, ori, color = [0.1, 1, 0.1], static = False)
-                pos_0, ori_0 = self._get_state()
-                pos_0 = pos_0[np.newaxis, :]
-                ori_0 = ori_0[np.newaxis, :]
-                self.pos = np.copy(pos_0) if obj == 0 else np.concatenate([self.pos, pos_0], 0)
-                self.ori = np.copy(ori_0) if obj == 0 else np.concatenate([self.ori, ori_0], 0)
-
-
+                # 上からCuboidを落とす
+                self._create_object(pos = pos, ori = ori, color = [0.1, 0.7, 0.1], static = False, obj = obj)
+                pos_0, ori_0 = self._get_state('Obj' + str(obj))
+                # Cuboidを消去してstatic=Trueにする。
+                Object.remove(Shape('Obj' + str(obj)))
+                color = [0.1, 0.1, 1] if self.num_obj == obj + 1 else [0.1, 1, 0.1]
+                self._create_object(pos = pos_0, ori = ori_0, color = color, static = True, obj = obj)
             response = input('>The arrangement is Ok? (yes or no)')
             if response == 'yes':
-                self.pr.stop()
-                self.pr.start()
                 self.done = True
             else:
                 self.reset()
-        print('pos:', self.pos)
-        print('ori:', self.ori)
-        self._create_arrangement(self.pos, self.ori)
+                
+        [self.pr.step() for _ in range(100)]
 
-
-    def _create_object(self, pos, ori, color, static):
-        size = [0.02, 0.045, 0.2]
-        pos = list(np.random.uniform(self.position_min, self.position_max))
-        ori = list(np.random.uniform(self.orientation_min, self.orientation_max))
+    def _create_object(self, pos, ori, color, static, obj):
+        size = [0.02, 0.05, 0.15]
         Arrangement_obj = Shape.create(type = PrimitiveShape.CUBOID,
-                                       mass = 0.0001,
+                                       mass = 0.01,
                                        size = size,
                                        color = color,
                                        static = static, respondable=True)
+        Arrangement_obj.set_name('Obj' + str(obj))
+        Arrangement_obj.set_bullet_friction(1.)
         Arrangement_obj.set_position(pos)
         Arrangement_obj.set_orientation(ori)
-        [self.pr.step() for _ in range(500)]
-
-    def _create_arrangement(self, pos, ori):
-        for Arr_pos, Arr_ori in zip(pos[:self.num_arrange + 1], ori[:self.num_arrange + 1]):
-            color = [0.1, 1, 0.1] if self.num_obj == self.num_arrange + 1 else [0.1, 0.1, 1]
-            self._create_object(pos, ori, color, static = True)
-            self.num_arrange += 1
-
-
+        [self.pr.step() for _ in range(100)]
+        
     def reset(self):
-        self.num_arrange = 0
-        self.pos = np.array([])
-        self.ori = np.array([])
         self.pr.stop()
         self.pr.start()
-
 
 if __name__ == '__main__':
     scene = 'assets/collision_detection.ttt'
     abs_scene = osp.join(osp.dirname(osp.abspath(__file__)), scene)
-
     env = CollisionDetectionEnv(scene_file=abs_scene, headless=False, num_obj=10)
-    done = False
-    step = 0
 
     env._collision_detection()
 
     env.shutdown()
-
